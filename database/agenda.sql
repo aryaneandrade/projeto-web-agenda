3af95529165872bcc905886617dcca95732d52ad
Aryane <aryaneands@gmail.com>
2025-11-14 10:41:09 -0300

    adicionando arquivo .sql para a criação do banco de dados

---
+++ database/agenda.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE agenda;
+
+USE agenda;
+
+CREATE TABLE contacts (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(150),
+    phone VARCHAR(20),
+    observations TEXT
+);
